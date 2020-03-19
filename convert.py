2efd7313c414573a1585373583281e8798d41b0e
Ryan Riley <rriley.cs@gmail.com>
2020-03-19 15:36:06 +0300

    Moving output to one file instead of two and tweaking how name is printed

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
 import csv
 
-header = """
+fileheader = """
 \\UseRawInputEncoding
 \\documentclass{article}
 \\usepackage{fullpage}
@@ -11,8 +11,10 @@ header = """
 
 \\pagestyle{empty}
 \\begin{document}
+"""
 
-{\Large %s}
+header = """
+{\Large Name: %s}
 """
 
 q_template = """
@@ -26,7 +28,7 @@ q_template = """
 \\end{lstlisting}
 """
 
-footer = """
+filefooter = """
 \\end{document}
 """
 
@@ -34,35 +36,41 @@ with open("answers.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     questions = []
-    for row in csv_reader:
-        if line_count == 0:
-            # This gets just the questions
-            qs = row[9:-4:2]
-            for q in qs:
-                # Ditch the number and colon on the front
-                l = q.find(": ")
-                q = q[l+2:]
-                questions.append(q)
-            
-            with open(f"output/blank.tex","w") as f:
-                f.write(header%("Name"))
-                
-                for i in range(len(questions)):                    
-                    f.write(q_template%(questions[i], ""))
-                    
-                f.write(footer)            
-        else:
-            idnum = row[1]
-            answers = row[9:-4:2]
-            with open(f"output/{idnum}.tex","w") as f:
-                f.write(header%(row[0]))
-                
-                for i in range(len(questions)):                    
-                    f.write(q_template%(questions[i], answers[i]))
-                    
-                f.write(footer)
-                
-        line_count += 1
+
+    with open("output/submissions.tex","w") as out:
         
-    print(questions)
+        out.write(fileheader)
+
+        for row in csv_reader:
+            if line_count == 0:
+                # This gets just the questions
+                qs = row[9:-4:2]
+                for q in qs:
+                    # Ditch the number and colon on the front
+                    l = q.find(": ")
+                    q = q[l+2:]
+                    questions.append(q)
+                
+                with open(f"output/blank.tex","w") as f:
+                    f.write(fileheader)
+                    f.write(header%(""))
+                    
+                    for i in range(len(questions)):                    
+                        f.write(q_template%(questions[i], ""))
+                        
+                    f.write(filefooter)
+            else:
+                idnum = row[1]
+                answers = row[9:-4:2]                
+                out.write(header%(row[0]))
+                
+                for i in range(len(questions)):                    
+                    out.write(q_template%(questions[i], answers[i]))
+
+                out.write("\\newpage")
+            
+            line_count += 1                        
+                    
+        out.write(filefooter)
+                    
             
